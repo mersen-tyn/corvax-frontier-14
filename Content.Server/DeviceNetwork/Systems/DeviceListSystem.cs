8cebe873cec4b18d065d8a12d695f1760fac93ed
Kill_Me_I_Noobs <118206719+Vonsant@users.noreply.github.com>
2024-05-02 00:53:00 +0300

    Update DeviceListSystem.cs

--- Content.Server/DeviceNetwork/Systems/DeviceListSystem.cs
+++ Content.Server/DeviceNetwork/Systems/DeviceListSystem.cs
@@ -5,8 +5,6 @@ using Content.Shared.DeviceNetwork.Components;
 using Content.Shared.DeviceNetwork.Systems;
 using Content.Shared.Interaction;
 using JetBrains.Annotations;
-using Robust.Shared.GameObjects;
-using Robust.Shared.IoC;
 using Robust.Shared.Map.Events;
 
 namespace Content.Server.DeviceNetwork.Systems;
@@ -15,6 +13,7 @@ namespace Content.Server.DeviceNetwork.Systems;
 public sealed class DeviceListSystem : SharedDeviceListSystem
 {
     [Dependency] private readonly NetworkConfiguratorSystem _configurator = default!;
+    private readonly List<DeviceListProcessingData> _processingData = new();
 
     public override void Initialize()
     {
@@ -23,8 +22,8 @@ public sealed class DeviceListSystem : SharedDeviceListSystem
         SubscribeLocalEvent<DeviceListComponent, BeforeBroadcastAttemptEvent>(OnBeforeBroadcast);
         SubscribeLocalEvent<DeviceListComponent, BeforePacketSentEvent>(OnBeforePacketSent);
         SubscribeLocalEvent<BeforeSaveEvent>(OnMapSave);
-        // Subscribe to EntityTerminatingEvent
-        SubscribeLocalEvent<EntityTerminatingEvent>(OnEntityTerminating);
+        SubscribeLocalEvent<RoundEndTextEvent>(OnRoundEnd);
+        SubscribeLocalEvent<GenericEvent>(OnDeviceListProcessed);
     }
 
     private void OnShutdown(EntityUid uid, DeviceListComponent component, ComponentShutdown args)
@@ -43,198 +42,51 @@ public sealed class DeviceListSystem : SharedDeviceListSystem
         component.Devices.Clear();
     }
 
-    /// <summary>
-    /// Gets the given device list as a dictionary
-    /// </summary>
-    /// <remarks>
-    /// If any entity in the device list is pre-map init, it will show the entity UID of the device instead.
-    /// </remarks>
-    public Dictionary<string, EntityUid> GetDeviceList(EntityUid uid, DeviceListComponent? deviceList = null)
+    private void OnGetState(EntityUid uid, DeviceListComponent component, ref ComponentGetState args)
     {
-        if (!Resolve(uid, ref deviceList))
-            return new Dictionary<string, EntityUid>();
-
-        var devices = new Dictionary<string, EntityUid>(deviceList.Devices.Count);
-
-        foreach (var deviceUid in deviceList.Devices)
-        {
-            if (!TryComp(deviceUid, out DeviceNetworkComponent? deviceNet))
-                continue;
-
-            var address = MetaData(deviceUid).EntityLifeStage == EntityLifeStage.MapInitialized
-                ? deviceNet.Address
-                : $"UID: {deviceUid.ToString()}";
-
-            devices.Add(address, deviceUid);
-
-        }
-
-        return devices;
+        _processingData.Add(new DeviceListProcessingData(uid, component, args));
     }
 
-    /// <summary>
-    /// Checks if the given address is present in a device list
-    /// </summary>
-    /// <param name="uid">The entity uid that has the device list that should be checked for the address</param>
-    /// <param name="address">The address to check for</param>
-    /// <param name="deviceList">The device list component</param>
-    /// <returns>True if the address is present. False if not</returns>
-    public bool ExistsInDeviceList(EntityUid uid, string address, DeviceListComponent? deviceList = null)
+    private void OnDeviceListProcessed(GenericEvent ev)
     {
-        var addresses = GetDeviceList(uid).Keys;
-        return addresses.Contains(address);
-    }
-
-    /// <summary>
-    /// Filters the broadcasts recipient list against the device list as either an allow or deny list depending on the components IsAllowList field
-    /// </summary>
-    private void OnBeforeBroadcast(EntityUid uid, DeviceListComponent component, BeforeBroadcastAttemptEvent args)
-    {
-        //Don't filter anything if the device list is empty
-        if (component.Devices.Count == 0)
-        {
-            if (component.IsAllowList)
-                args.Cancel();
-            return;
-        }
-
-        HashSet<DeviceNetworkComponent> filteredRecipients = new(args.Recipients.Count);
-
-        foreach (var recipient in args.Recipients)
-        {
-            if (component.Devices.Contains(recipient.Owner) == component.IsAllowList)
-                filteredRecipients.Add(recipient);
-        }
-
-        args.ModifiedRecipients = filteredRecipients;
-    }
-
-    /// <summary>
-    /// Filters incoming packets if that is enabled <see cref="OnBeforeBroadcast"/>
-    /// </summary>
-    private void OnBeforePacketSent(EntityUid uid, DeviceListComponent component, BeforePacketSentEvent args)
-    {
-        if (component.HandleIncomingPackets && component.Devices.Contains(args.Sender) != component.IsAllowList)
-            args.Cancel();
-    }
-
-    public void OnDeviceShutdown(Entity<DeviceListComponent?> list, Entity<DeviceNetworkComponent> device)
-    {
-        device.Comp.DeviceLists.Remove(list.Owner);
-        if (!Resolve(list.Owner, ref list.Comp))
+        if (ev.Event != DeviceListProcessedEvent)
             return;
 
-        list.Comp.Devices.Remove(device);
-        Dirty(list);
-    }
-
-    private void OnMapSave(BeforeSaveEvent ev)
-    {
-        List<EntityUid> toRemove = new();
-        var query = GetEntityQuery<TransformComponent>();
-        var enumerator = AllEntityQuery<DeviceListComponent, TransformComponent>();
-        while (enumerator.MoveNext(out var uid, out var device, out var xform))
+        foreach (var data in _processingData)
         {
-            if (xform.MapUid != ev.Map)
-                continue;
-
-            foreach (var ent in device.Devices)
+            var component = data.Component;
+            var args = data.Args;
+            var entities = new HashSet<EntityUid>(component.Devices.Count);
+            foreach (var device in component.Devices)
             {
-                if (!query.TryGetComponent(ent, out var linkedXform))
-                {
-                    // Entity was deleted.
-                    // TODO remove these on deletion instead of on-save.
-                    toRemove.Add(ent);
-                    continue;
-                }
-
-                if (linkedXform.MapUid == ev.Map)
-                    continue;
-
-                toRemove.Add(ent);
-                // TODO full game saves.
-                // when full saves are supported, this should instead add data to the BeforeSaveEvent informing the
-                // saving system that this map (or null-space entity) also needs to be included in the save.
-                Log.Error(
-                    $"Saving a device list ({ToPrettyString(uid)}) that has a reference to an entity on another map ({ToPrettyString(ent)}). Removing entity from list.");
+                if (EntityManager.TryGetComponent(device, out MetaDataComponent? meta))
+                    entities.Add(device);
             }
 
-            if (toRemove.Count == 0)
-                continue;
-
-            var old = device.Devices.ToList();
-            device.Devices.ExceptWith(toRemove);
-            RaiseLocalEvent(uid, new DeviceListUpdateEvent(old, device.Devices.ToList()));
-            Dirty(uid, device);
-            toRemove.Clear();
+            args.State = new DeviceListComponentState(entities, component.IsAllowList, component.HandleIncomingPackets);
         }
+
+        _processingData.Clear();
     }
 
-    /// <summary>
-    ///     Updates the device list stored on this entity.
-    /// </summary>
-    /// <param name="uid">The entity to update.</param>
-    /// <param name="devices">The devices to store.</param>
-    /// <param name="merge">Whether to merge or replace the devices stored.</param>
-    /// <param name="deviceList">Device list component</param>
-    public DeviceListUpdateResult UpdateDeviceList(EntityUid uid, IEnumerable<EntityUid> devices, bool merge = false, DeviceListComponent? deviceList = null)
+    private const string DeviceListProcessedEvent = "DeviceListProcessed";
+
+    private void OnRoundEnd(RoundEndTextEvent ev)
     {
-        if (!Resolve(uid, ref deviceList))
-            return DeviceListUpdateResult.NoComponent;
-
-        var list = devices.ToList();
-        var newDevices = new HashSet<EntityUid>(list);
-
-        if (merge)
-            newDevices.UnionWith(deviceList.Devices);
-
-        if (newDevices.Count > deviceList.DeviceLimit)
-        {
-            return DeviceListUpdateResult.TooManyDevices;
-        }
-
-        var query = GetEntityQuery<DeviceNetworkComponent>();
-        var oldDevices = deviceList.Devices.ToList();
-        foreach (var device in oldDevices)
-        {
-            if (newDevices.Contains(device))
-                continue;
-
-            deviceList.Devices.Remove(device);
-            if (query.TryGetComponent(device, out var comp))
-                comp.DeviceLists.Remove(uid);
-        }
-
-        foreach (var device in newDevices)
-        {
-            if (!query.TryGetComponent(device, out var comp))
-                continue;
-
-            if (!deviceList.Devices.Add(device))
-                continue;
-
-            comp.DeviceLists.Add(uid);
-        }
-
-        RaiseLocalEvent(uid, new DeviceListUpdateEvent(oldDevices, list));
-
-        Dirty(uid, deviceList);
-
-        return DeviceListUpdateResult.UpdateOk;
+        RaiseLocalEvent(new GenericEvent(DeviceListProcessedEvent));
     }
 
-    /// <summary>
-    ///  Corrected event handler to remove deleted entities from device lists
-    /// </summary>
-    private void OnEntityTerminating(ref EntityTerminatingEvent ev)
+    public sealed class DeviceListProcessingData
     {
-        var query = GetEntityQuery<DeviceListComponent>();
-        foreach (var comp in query) // Iterate through each DeviceListComponent
+        public EntityUid Uid { get; }
+        public DeviceListComponent Component { get; }
+        public ComponentGetState Args { get; }
+
+        public DeviceListProcessingData(EntityUid uid, DeviceListComponent component, ComponentGetState args)
         {
-            if (comp.Devices.Remove(ev.EntityUid)) // Check and remove the terminating entity
-            {
-                Dirty(comp.Owner, comp);
-            }
+            Uid = uid;
+            Component = component;
+            Args = args;
         }
-    } 
+    }
 }
